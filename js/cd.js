d5e6584134d69023f73a19d79a0814f1e119734b
AnnaAguilar <34386951+AnnaAguilar@users.noreply.github.com>
2018-07-04 15:29:11 -0300

    funcao
    agora passa em todos os botoes

--- js/cd.js
+++ js/cd.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     entrada = window.location.search.replace('?', '');
 
-    let a = document.querySelectorAll('aside a:nth-child(n+2)');
+    let a = document.querySelectorAll('aside a:nth-child(n+2), nav a');
     for (var i = 0; i < a.length; i++) {
         a[i].href += window.location.search;
     }
